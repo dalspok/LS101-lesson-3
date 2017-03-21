6599d50eafe6f0296aad8db34c9239af359e6718
Dalibor Spok <dalspok@gmail.com>
2017-03-21 15:43:04 +0100

    medium_1 iter 1

---
+++ easy_3_i2.rb
@@ -0,0 +1,55 @@
+# 10.times { |spaces| puts " " * spaces + "The Flintstones Rock!"  }
+# 0.upto(9) { |spaces| puts "#{" "*spaces}The Flintstones Rock!"  }
+
+# 2
+
+# puts "the value of 40 + 2 is #{(40 + 2)}"
+# puts "the value of 40 + 2 is " + (40 + 2).to_s
+
+# 3
+
+# def factors(number)
+#   dividend = number
+#   divisors = []
+#   while dividend > 0
+#     divisors << number / dividend if number % dividend == 0
+#     dividend -= 1
+#   end
+#   divisors
+# end
+
+# puts factors(24)
+
+# 5
+
+# limit = 15
+
+# def fib(first_num, second_num, limit)
+#   while second_num < limit
+#     sum = first_num + second_num
+#     first_num = second_num
+#     second_num = sum
+#   end
+#   sum
+# end
+
+# result = fib(0, 1, limit)
+# puts "result is #{result}"
+
+# 7
+
+# 34
+
+# 9
+
+# paper
+
+# 10
+
+# no
+
+
+
+
+
+
